a3ae09f54f4178fc65ac2ffd4878c364cb68a321
Kamil <40739398+KamlaXPL@users.noreply.github.com>
2021-08-04 21:39:35 +0200

    Update User.java

--- src/main/java/pl/kamlax/knockout/user/User.java
+++ src/main/java/pl/kamlax/knockout/user/User.java
@@ -3,6 +3,7 @@ package pl.kamlax.knockout.user;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.Data;
 
 import java.util.UUID;
 
@@ -13,6 +14,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Getter
 @Setter
+@Data
 public final class User {
     private final UUID playerUUID;
     private long time, rescueTime;
